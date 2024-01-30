500fd6b5af1b2f7a74b1269b93950018b5347824
Dwijasri <157499031+Dwijasri@users.noreply.github.com>
2024-01-30 20:39:53 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import WelcomeMessage from "./components/welcome";
+import StudentInfo from "./components/student-info";
+function App() {
+ return (
+ <div className="App">
+ <h1>Welcome, World!</h1>
+ <p>Name : Dwijasri</p>
+ <p>Roll No. : 2211CS010223</p>
+ <p>CGPA : 9.3</p>
+ <h1>Skills</h1>
+ <op>1.Drawing</op>
+ <op>2.Reading</op>
+ </div>
+ );
+}
+export default App; 
---
+++ student-info.js
@@ -0,0 +1,20 @@
+import React from 'react';
+const StudentInfo = () => {
+ return (
+ <div>
+ <table>
+ <tbody>
+ <tr>
+ <td>Roll No.</td>
+ <td>2211CS010223</td>
+ </tr>
+ <tr>
+ <td>Name</td>
+ <td>DWIJASRI</td>
+ </tr>
+ </tbody>
+ </table>
+ </div>
+ );
+};
+export default StudentInfo;
